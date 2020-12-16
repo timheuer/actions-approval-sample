4e0d0b93d97238b747bd66e211a3d069d0e86dd9
Tim Heuer <tim@timheuer.com>
2020-12-15 21:51:52 -0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Adding approval flow to GitHub Actions
+This is a sample repo that shows how to add a staging -> production workflow with approvals in your GitHub Actions deployment.
+
+Read more about this at [Add approval workflow to your GitHub Actions](https://timheuer.com/blog/add-approval-workflow-to-github-actions)
---
+++ sample-workflow-deploy/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample_workflow_deploy.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Environment"] = _configuration["APPSERVICE_ENVIRONMENT"];
+        }
+    }
+}
